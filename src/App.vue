ac9f471d55bf0461912bfaa213d127bc0a2cbd91
KieranWaigel <30296791+KieranWaigel@users.noreply.github.com>
2020-12-20 13:25:56 -0500

    Login Page 1.0
    Testing basic login page and webapp compatibility

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,58 @@
 <template>
-  <div>Hello {{ value }}</div>
+<div>
+  
+   <head>
+      <title>juiceromc: the juicer of the future</title>
+    </head>
+
+    <div id="juicero">
+
+      <body>
+        <div id="login">
+          <h3>
+            &lt;juiceromc&gt;
+          </h3>
+          <div id="logininput">
+
+            <input v-model="usr" placeholder="username">
+            <input v-model="pwd" placeholder="password">
+            <button id="usrbtn" @click="addUser"> Add User</button>
+
+          </div>
+        </div>
+        <div id="content">
+
+          <h3>
+            &lt;current users&gt;
+          </h3>
+          <li v-for="name in users" v-bind:key="name"></li>
+        </div>
+
+      </body>
+    </div>
+</div>
 </template>
 
 <script>
 export default {
-  name: "App",
-  data() {
-    return {
-      value: "World"
-    };
-  }
+  el: '#juicero',
+  data: function() {
+return {
+    usr: '',
+    users: ["Gary"]
+  };
+},
+
+  methods: {
+
+    addUser() {
+      this.users.push(this.usr);
+      this.usr = '';
+    }
+  },
 };
 </script>
+
+<style>
+  @import '/assets/stylemain.css';
+</style>
